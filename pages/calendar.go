d4345bbd3a7d472860d927fcab621841a9729e77
Brandon Mercado <brandon.mercado.264@my.csun.edu>
2025-08-05 21:01:58 -0700

    setting layout for calendar page

--- pages/calendar.go
+++ pages/calendar.go
@@ -8,12 +8,37 @@ import (
 )
 
 func CalendarPage(app *tview.Application, returnTo func()) tview.Primitive {
-	view := tview.NewTextView().
+	header := tview.NewTextView().
 		SetTextAlign(tview.AlignCenter).
 		SetDynamicColors(true).
 		SetText("📅 [::b]Calendar View[::-]\n\n[gray]Press Esc to return")
 
-	view.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+	calendarBox := tview.NewBox().SetBorder(true).SetTitle("[ CALENDAR ]")
+
+	dailyScheduleMini := tview.NewBox().SetBorder(true).SetTitle("[ TODAY'S SCHEDULE ]")
+
+	leftPadding := tview.NewBox()
+	middlePadding := tview.NewBox()
+	rightPadding := tview.NewBox()
+
+	mainBody := tview.NewFlex().SetDirection(tview.FlexColumn).
+		AddItem(leftPadding, 0, 1, false).
+		AddItem(calendarBox, 0, 5, false).
+		AddItem(middlePadding, 0, 1, false).
+		AddItem(dailyScheduleMini, 0, 2, false).
+		AddItem(rightPadding, 0, 1, false)
+
+	footer := tview.NewTextView().
+		SetTextAlign(tview.AlignCenter).
+		SetDynamicColors(true).
+		SetText("FOOTER FOOTER FOOTER")
+
+	page := tview.NewFlex().SetDirection(tview.FlexRow).
+		AddItem(header, 0, 2, false).
+		AddItem(mainBody, 0, 5, false).
+		AddItem(footer, 0, 1, false)
+
+	page.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		if event.Key() == tcell.KeyEsc {
 			returnTo()
 			return nil
@@ -21,5 +46,5 @@ func CalendarPage(app *tview.Application, returnTo func()) tview.Primitive {
 		return event
 	})
 
-	return view
+	return page
 }
